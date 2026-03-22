ff155931354cbe23149810fb1af6dcb56580fb60
FernandoGtz <fernandogtz242@gmail.com>
2026-03-21 19:56:49 -0600

    Subida de proyecto POC

---
+++ src/main/java/com/BMG_System_POC/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.BMG_System_POC.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
